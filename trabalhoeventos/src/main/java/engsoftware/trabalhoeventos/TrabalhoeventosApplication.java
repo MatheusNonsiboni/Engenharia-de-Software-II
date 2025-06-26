db8510142e8468a6c9d4dd2912de7ce288e9b22b
LiviaKouketsu <liviakouketsu@gmail.com>
2025-06-26 15:47:31 -0300

    versão 1

---
+++ trabalhoeventos/src/main/java/engsoftware/trabalhoeventos/TrabalhoeventosApplication.java
@@ -0,0 +1,13 @@
+package engsoftware.trabalhoeventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoeventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoeventosApplication.class, args);
+	}
+
+}
