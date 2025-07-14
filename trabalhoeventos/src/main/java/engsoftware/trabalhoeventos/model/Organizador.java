c9aaea3c56d942cc6447fdf2589d2162207fa95c
JVS <joaovitors.397@gmail.com>
2025-07-14 18:27:19 -0300

    Adicionando Organizador DTO e ajustando outras coisas

--- trabalhoeventos/src/main/java/engsoftware/trabalhoeventos/model/Organizador.java
+++ trabalhoeventos/src/main/java/engsoftware/trabalhoeventos/model/Organizador.java
@@ -6,73 +6,27 @@ import org.springframework.beans.BeanUtils;
 
 @Entity
 @Table(name = "organizador")
-public class Organizador {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Organizador extends Usuario {
 
-    @NotBlank(message = "O nome do organizador é obrigatório")
-    private String nome;
-
-    @NotBlank(message = "O email do organizador é obrigatório")
-    private String email;
-
-    @NotBlank(message = "A senha é obrigatória")
-    private String senha;
-
-    @NotBlank(message = "O telefone do organizador é obrigatório")
-    private String telefone;
-
-    @NotBlank(message = "O username do organizador é obrigatório")
-    private String username;
-
-    @NotBlank(message = "O CPF PU CNPJ do organizador é obrigatório")
+    @NotBlank(message = "O CPF ou CNPJ do organizador é obrigatório")
     private String documento;
 
     public Organizador() {
-
+        super();
     }
 
-    public Long getId() {
-        return id;
+    public Organizador(OrganizadorDTO dto) {
+        super(dto);
+        this.documento = dto.getDocumento();
     }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getSenha() {
-        return senha;
-    }
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-    public String getTelefone() {
-        return telefone;
-    }
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
+
+
     public String getDocumento() {
         return documento;
     }
+
     public void setDocumento(String documento) {
         this.documento = documento;
     }
 }
+
--- trabalhoeventos/src/main/java/engsoftware/trabalhoeventos/model/Usuario.java
+++ trabalhoeventos/src/main/java/engsoftware/trabalhoeventos/model/Usuario.java
@@ -2,7 +2,6 @@ package engsoftware.trabalhoeventos.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import java.util.List;
 
 @Entity
 public class Usuario {
@@ -12,15 +11,11 @@ public class Usuario {
     private Long id;
 
     @NotBlank(message = "O nome do usuário é obrigatório")
-    private String nome_completo;
 
-    @NotBlank(message = "O email do usuário é obrigatório")
     private String email;
 
     @NotBlank(message = "A senha é obrigatória")
-    private String senha;
 
-    @NotBlank(message = "O telefone do usuário é obrigatório")
     private String telefone;
 
     public Long getId() {
@@ -29,11 +24,7 @@ public class Usuario {
     public void setId(Long id) {
         this.id = id;
     }
-    public String getNome_completo() {
-        return nome_completo;
     }
-    public void setNome_completo(String nome_completo) {
-        this.nome_completo = nome_completo;
     }
     public String getEmail() {
         return email;
@@ -53,4 +44,4 @@ public class Usuario {
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
-}
+    }
