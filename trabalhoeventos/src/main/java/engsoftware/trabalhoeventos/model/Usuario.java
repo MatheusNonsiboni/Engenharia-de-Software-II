55e002be539ebae89ddd6ff7b659b2eaa2fb31a0
LiviaKouketsu <liviakouketsu@gmail.com>
2025-07-15 22:26:11 -0300

    versão 1.4.2 (desfazendo mudanças)

--- trabalhoeventos/src/main/java/engsoftware/trabalhoeventos/model/Usuario.java
+++ trabalhoeventos/src/main/java/engsoftware/trabalhoeventos/model/Usuario.java
@@ -2,8 +2,8 @@ package engsoftware.trabalhoeventos.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import java.util.List;
 
-@Inheritance(strategy = InheritanceType.JOINED)
 @Entity
 public class Usuario {
 
@@ -11,44 +11,45 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "O nome do usuário é obrigatório")
+    private String nome_completo;
+
     @NotBlank(message = "O email do usuário é obrigatório")
     private String email;
 
     @NotBlank(message = "A senha é obrigatória")
     private String senha;
 
-    @NotBlank(message = "O telefone é obrigatório")
+    @NotBlank(message = "O telefone do usuário é obrigatório")
     private String telefone;
 
-    // Getters e Setters
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
+    public String getNome_completo() {
+        return nome_completo;
+    }
+    public void setNome_completo(String nome_completo) {
+        this.nome_completo = nome_completo;
+    }
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getSenha() {
         return senha;
     }
-
     public void setSenha(String senha) {
         this.senha = senha;
     }
-
     public String getTelefone() {
         return telefone;
     }
-
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
